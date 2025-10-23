9bd2dd955bf19f50df325a545b79e40f42206fcf
Mark Kim <fresh502@gmail.com>
2025-10-23 11:30:56 +0900

    seed and update users, products api

--- app.js
+++ app.js
@@ -16,7 +16,23 @@ app.post('/users', async (req, res) => {
 });
 
 app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany();
+  const { offset = 0, limit = 0, order = 'newest' } = req.query;
+  let orderBy;
+  switch (order) {
+    case 'oldest':
+      orderBy = { createdAt: 'asc' };
+      break;
+    case 'newest':
+      orderBy = { createdAt: 'desc' };
+      break;
+    default:
+      orderBy = { createdAt: 'desc' };
+  }
+  const users = await prisma.user.findMany({
+    orderBy,
+    skip: parseInt(offset),
+    take: parseInt(limit),
+  });
   res.send(users);
 });
 
@@ -61,7 +77,31 @@ app.post('/products', async (req, res) => {
 });
 
 app.get('/products', async (req, res) => {
-  const products = await prisma.product.findMany();
+  const { offset = 0, limit = 0, order = 'newest', category } = req.query;
+  let orderBy;
+  switch (order) {
+    case 'priceLowest':
+      orderBy = { price: 'asc' };
+      break;
+    case 'priceHighest':
+      orderBy = { price: 'desc' };
+      break;
+    case 'oldest':
+      orderBy = { createdAt: 'asc' };
+      break;
+    case 'newest':
+      orderBy = { createdAt: 'desc' };
+      break;
+    default:
+      orderBy = { createdAt: 'desc' };
+  }
+  const where = category ? { category } : {};
+  const products = await prisma.product.findMany({
+    where,
+    orderBy,
+    skip: parseInt(offset),
+    take: parseInt(limit),
+  });
   res.send(products);
 });
 
